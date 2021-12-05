1d7eb5ae0f2466e365c0b3608b6923ba03651ab7
Aziz Bakhovaddinov <bakhovaddinov200612@yandex.ru>
2021-12-05 15:36:16 +0300

    The init version of the app

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cmdNotes
+{
+    public class Program
+    {
+        static List<string> Notes = new List<string>();
+        static public void Main()
+        {
+            
+        }
+        private void NewNote()
+        {
+            string notetext = Console.ReadLine();
+            Program.Notes.Add(notetext);
+        }
+    }
+
+}
