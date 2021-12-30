a071c7332339dd6e7d51fca9f4fe2cc9040aab50
Aziz Bakhovaddinov <bakhovaddinov200612@yandex.ru>
2021-12-30 13:06:42 +0300

    added an option to add notes and to display them

--- Program.cs
+++ Program.cs
@@ -5,14 +5,44 @@ namespace cmdNotes
     public class Program
     {
         static List<string> Notes = new List<string>();
-        static public void Main()
-        {
-            
-        }
-        private void NewNote()
+        
+        private static void NewNote()
         {
             string notetext = Console.ReadLine();
             Program.Notes.Add(notetext);
+            
+        }
+        private static void PrintNotes()
+        {
+            int count = 1;
+            Console.WriteLine("----------------");
+            foreach (string note in Program.Notes)
+            {
+                Console.WriteLine($"{count}: {note}");
+                count++;
+            }
+            Console.WriteLine("----------------");
+        }
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Choose an action\nA - Add a note\nP - Print all current notes\n");
+                string userChoice = Console.ReadLine();
+                if (userChoice.ToUpper() == "A")
+                {
+                    Console.WriteLine("Type your note:");
+                    NewNote();
+                }
+                else if (userChoice.ToUpper() == "P")
+                {
+                    PrintNotes();
+                }
+                else
+                {
+                    Console.Write("-------------\nInvalid Input\n-------------\n");
+                }
+            }
         }
     }
 
